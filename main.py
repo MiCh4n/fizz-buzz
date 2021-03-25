41c992cb26694454b7ff96bd6279b743c7a8c1e9
Julian Dobrosielski <48097351+MiCh4n@users.noreply.github.com>
2021-03-25 22:10:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+i = 1
+while i <= 100:
+    if i % 15 == 0:
+        print(str(i)+" FizzBuzz!")
+    elif i % 3 == 0:
+        print(str(i)+" Fizz!")
+    elif i % 5 == 0:
+        print(str(i)+" Buzz!")
+    i += 1
